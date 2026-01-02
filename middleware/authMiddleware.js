143400a9f226a71beedab828c06f2641b86679e5
AloneFFOfficial <chaitanyalohani175@gmail.com>
2026-01-02 15:29:09 +0530

    add basic jwt authentication flow

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+
+export const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Access denied. No token." });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.jwt_secret);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(400).json({ error: 'Invalid or expired token.' });
+    }
+
+};   
+
